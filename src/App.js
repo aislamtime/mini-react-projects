eee84a38b2339bfe28a952f9910b76e09d5c8ffa
aislamtime <116115530+aislamtime@users.noreply.github.com>
2023-02-12 16:33:24 +0300

    add currencey-convertor

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { Block } from './Block'
+import './index.scss'
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState('USD')
+  const [toCurrency, setToCurrency] = React.useState('RUB')
+  const [fromPrice, setFromPrice] = React.useState('')
+  const [toPrice, setToPrice] = React.useState('')
+
+  const ratesRef = React.useRef({})
+
+  React.useEffect(() => {
+    fetch('https://www.cbr-xml-daily.ru/latest.js')
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current = { ...json.rates, RUB: 1 }
+        onChangeFromPrice(1)
+      })
+      .catch((error) => {
+        console.warn(error)
+        alert('Не удалось получить курс валют с сервера :(')
+      })
+  }, [])
+
+  const onChangeFromPrice = (value) => {
+    setFromPrice(value)
+    if (fromCurrency === toCurrency) setToPrice(value)
+    else {
+      const price = value / ratesRef.current[fromCurrency]
+      const result = price * ratesRef.current[toCurrency]
+      setToPrice(result.toFixed(3))
+    }
+  }
+  const onChangeToPrice = (value) => {
+    setToPrice(value)
+    if (fromCurrency === toCurrency) setFromPrice(value)
+    else {
+      const price = value / ratesRef.current[toCurrency]
+      const result = price * ratesRef.current[fromCurrency]
+      //const price1 = (ratesRef.current[toCurrency] / ratesRef.current[fromCurrency]) * value
+      setFromPrice(result.toFixed(3))
+    }
+  }
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency])
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={(cur) => setFromCurrency(cur)}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={(cur) => setToCurrency(cur)}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   )
 }
