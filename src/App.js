44b9fbfbbc92bfb980071f35a528ab85d781591c
aislamtime <116115530+aislamtime@users.noreply.github.com>
2023-02-13 00:48:42 +0300

    add photos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg'
-import './App.css'
-// https://63d12d27120b32bbe8f2dbf8.mockapi.io/collection_for_mini_progects
+import React from 'react'
+import './index.scss'
+
+import { Collection } from './components/Collection'
+
 function App() {
+  const [collections, setCollections] = React.useState([])
+  const [category, setCategory] = React.useState(0)
+  const [searchValue, setSearchValue] = React.useState('')
+  const [page, setPage] = React.useState(1)
+
+  const pages = [1, 2, 3]
+
+  const allCatogories = ['Все', 'Горы', 'Море', 'Архитектура', 'Города']
+
+  React.useEffect(() => {
+    fetch(
+      `https://63d12d27120b32bbe8f2dbf8.mockapi.io/collection_for_mini_progects?${
+        category !== 0 ? 'category=' + category : ''
+      }&p=${page}&l=3`,
+    )
+      .then((res) => res.json())
+      .then((json) => setCollections(json))
+      .catch((error) => {
+        console.warn(error)
+        alert('Произошла ошибка при получении колекции :(')
+      })
+  }, [category, page])
+
+  console.log(collections)
+
+  const onChangeCategory = (index) => setCategory(index)
+
+  //const tryCollections =
+  //  category === 0 ? collections : collections.filter((item) => item.category === category)
+
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <h1>Моя коллекция фотографий</h1>
+      <div className='top'>
+        <ul className='tags'>
+          {allCatogories.map((item, i) => (
+            <li
+              key={i}
+              onClick={() => onChangeCategory(i)}
+              className={category === i ? 'active' : ''}>
+              {item}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.currentTarget.value)}
+          className='search-input'
+          placeholder='Поиск по названию'
+        />
+      </div>
+      <div className='content'>
+        {collections
+          .filter((item) => item.name.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((item) => (
+            <Collection key={item.name} name={item.name} images={item.photos} />
+          ))}
+      </div>
+      <ul className='pagination'>
+        {pages.map((item) => (
+          <li key={item} onClick={() => setPage(item)} className={page === item ? 'active' : ''}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
